4a88e9516a5b21d41aac84940c6b5781b9353139
yoonsery <69155242+yoonsery@users.noreply.github.com>
2021-04-28 21:55:55 +0900

    upload files

---
+++ main.js
@@ -0,0 +1,51 @@
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+function onAdd() {
+  const text = input.value;
+  if (text === '') {
+    input.focus();
+    return;
+  }
+
+  const item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView({ block: 'center' });
+  input.value = '';
+  input.focus();
+}
+
+let id = 0;
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+        <div class="item">
+            <span class="item__name">${text}</span>
+            <button class="item__delete">
+                <i class="fas fa-trash" data-id=${id}></i>
+            </button>
+        </div>`;
+  id++;
+  return itemRow;
+}
+
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
+
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    onAdd();
+  }
+});
+
+items.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+  }
+});
